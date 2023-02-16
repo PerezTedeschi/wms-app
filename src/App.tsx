e33ec62af0d471e89859cbc231c51e197fa62122
PerezTedeschi <santiagopereztedeschi@hotmail.com>
2023-02-16 18:39:48 -0300

    5: Add home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Menu from './components/Menu';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <>
-      <Menu/>
-      <h1>wms App</h1>
-    </>);
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
